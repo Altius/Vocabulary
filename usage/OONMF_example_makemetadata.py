8ad007ef0931143cd01dc6834e1b3251aee6d339
mooratov <mooratov@gmail.com>
2019-01-15 11:41:12 -0800

    updating metadata usage

--- usage/OONMF_example_makemetadata.py
+++ usage/OONMF_example_makemetadata.py
@@ -1,35 +1,75 @@
+# use OONMFmetadata library to make our signature category statistical association plots
+
 import numpy as np
 import pandas as pd
+import sys
 
 import OONMF
-from OONMFhelpers import *
-from OONMFmetadata import *
+import OONMFhelpers as OH
+import OONMFmetadata as OMD
 
-myNMFcomps=17
+today = OH.get_today()
 
-finname = 'ENCODE 651-sample list WM20170918 - ENCODE3 metadata.csv'
-MetaData  = pd.read_csv(finname)
-MetaData = MetaData[0:827]
+myNC=16
+usingCut = False
+thecut = []
 
+if len(sys.argv) > 1:
+    finname_base = sys.argv[1]
+    myNC=int(sys.argv[2])
+
+if len(sys.argv)> 3:
+    print('adding cut')
+    usingCut = True
+    cut_finname = sys.argv[3]
+    thecut = np.load(cut_finname)
+    
+    
+myNMFcomps=myNC
+
+finname = 'ENCODE733MetaData.csv'
+MetaData  = pd.read_csv(finname, sep='\t')
+print (MetaData.head())
 
 a = OONMF.NMFobject(theNcomps=myNMFcomps)
-a.Basis_Names = pd.read_table('colTitles.651samples_friendly.txt', header=None)[1].values
+sampnamePD = pd.read_table('sampnams_733.txt', header=None, names=['LN', 'DS', 'type'])
+sampnamePD['full_name'] = sampnamePD.LN + '-' + sampnamePD.DS + '-' + sampnamePD.type
 
-a.matrix_input_name(Basis_finname='92517/2017-09-25NMF_Ncomps'+str(myNMFcomps)+'Basis.npy', Mixture_finname='92517/2017-09-25NMF_Ncomps'+str(myNMFcomps)+'Mixture.npy')
+thenames = sampnamePD.full_name.values
+fullBasis_Names = thenames
+if (usingCut):
+    print('adjusting names')
+    thenames = thenames[thecut]
+a.Basis_Names = thenames
+rosetta = OMD.get_rosetta(MetaData, fullBasis_Names)
+
+
+print(a.Basis_Names[0:5])
+
+
+foutname_base = finname_base+today+'_'  
+
+a.matrix_input_name(Basis_finname=finname_base+'Basis.npy', Mixture_finname=finname_base+'Mixture.npy')
 
 a.read_matrix_input()
 
-homogeneity = []
-for sample in a.Basis:
-    hom = np.max(sample)/np.sum(sample)
-    homogeneity.append(hom)
-homogeneity = np.array(homogeneity)
 
-rosetta = get_rosetta(MetaData, a.Basis_Names)
+if (usingCut):
+    print('a' ,MetaData.shape, len(rosetta), len(thecut))
+    newMetaData = MetaData.loc[rosetta][thecut]
+    print('b', len(newMetaData))
+    newrosetta = np.arange(len(newMetaData)).astype(int)
 
-categories = ['system', 'organ','Biological_state','subsystem','Sex','germ layer', 'Growth stage']
+else:
+    newMetaData = MetaData
+    newrosetta = rosetta
+
+# all categories that have proven interesting, and then some
+categories = ['system', 'organ','Biological_state','subsystem','Sex','germ layer', 'Growth stage', 'class', 'lib_kit_method', 'Sample_group', 'Candidate removal -- insert sizes & dupe rate', 'Candidate removal -- insert sizes', 'Donor_ID', 'Ethnicity', 'lib_cleanup', 'sample_dhs_protease_inhibitor']
 
 for my_cat_str in categories:
     MetaData[my_cat_str].fillna('None', inplace=True)
-    (SystemChart,SystemTypes) = make_significance_plot(MetaData, a.Basis, my_cat_str, rosetta,thecmap='inferno', NMFCOMPS=myNMFcomps)
-    make_significance_plot_homogeneity(MetaData, homogeneity, my_cat_str, rosetta, thecmap='inferno', NMFCOMPS=myNMFcomps)
+    (SystemChart,SystemTypes) = OMD.make_significance_plot(newMetaData, a.Basis, my_cat_str, newrosetta,thecmap='inferno', NMFCOMPS=myNMFcomps,filename_addon=foutname_base, write_mode=True)
+    
+    # at the moment, homogeneity is not very useful / defunct 
+    #make_significance_plot_homogeneity(MetaData, homogeneity, my_cat_str, rosetta, thecmap='inferno', NMFCOMPS=myNMFcomps)
