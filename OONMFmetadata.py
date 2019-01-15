282af74026387b1a5cdf8dcb92a39af391b920cf
mooratov <mooratov@gmail.com>
2019-01-14 20:15:31 -0800

    updated OONMF metadata

--- OONMFmetadata.py
+++ OONMFmetadata.py
@@ -8,10 +8,22 @@ if (ClusterMode):
 	matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.stats import mannwhitneyu
-
 import pandas as pd
 
-def make_significance_plot(X, Basis, Category_str, my_rosetta, thecmap='viridis', NMFCOMPS=16, save=True, filename_addon=''):
+
+'''
+functions:
+
+make_significance_plot - make the signature system-wide association plots that we use to show statistical associations between components and metadata categories. 
+
+make_significance_plot_homogeneity - make a version of these metadata associations, but test for homogeneity among NMF components 
+
+get_rosetta - this is an important one. It figures out how to reorder the names of samples from metadata so that it matches up to the order of sample names from the masterlist matrix. Uses regular expressions. 
+
+'''
+
+
+def make_significance_plot(X, Basis, Category_str, my_rosetta, thecmap='viridis', NMFCOMPS=16, save=True, filename_addon='', PCAmode=False, write_mode=False):
     CategoryType = X[Category_str].value_counts().keys()[0:20]
     CategoryCats = X[Category_str].values[my_rosetta]
 
@@ -31,22 +43,33 @@ def make_significance_plot(X, Basis, Category_str, my_rosetta, thecmap='viridis'
             growthlist += [-1*np.log10(adjustedp)]
         list_of_sig.append(growthlist)
         
-    colnames = [Category_str.replace(" ", ""), 'Count'] + ['Comp'+str(i) for i in range(NMFCOMPS)]
+    colnames = [Category_str.replace(" ", ""), 'Count'] + ['Comp'+str(i+1) for i in range(NMFCOMPS)]
     
     CategoryChart = pd.DataFrame(list_of_sig, columns=colnames)
+    if write_mode:
+        CategoryChart.to_csv(filename_addon +  Category_str+'MWmatrix.csv', sep='\t') 
     CategoryChartMatrix = CategoryChart.values[:,2:].astype(float)
 
     plt.clf()
-    plt.figure(figsize=(30,len(CategoryType[0:20])*2))
+    
+    myfs = 45
+    plt.figure(figsize=(35,len(CategoryType[0:20])*2))
     plt.imshow(CategoryChartMatrix, cmap=thecmap, vmin=-3, vmax=30)
-    plt.xlabel('NMF component',fontsize=25)
-    plt.ylabel(Category_str,fontsize=25)
-    plt.yticks(np.arange(Ncats), CategoryType[0:20], rotation='horizontal',fontsize=25)
-    plt.xticks(np.arange(NMFCOMPS), np.arange(NMFCOMPS).astype(str), rotation='vertical',fontsize=25)
-    cbar = plt.colorbar()
-    cbar.set_label(r'- $\log_{10} (p*$'+str(NMFCOMPS)+r'$*$'+str(Ncats)+r'$)$',fontsize=25)
+    if (PCAmode):
+        plt.xlabel('Principal Component',fontsize=myfs)
+    else:
+        plt.xlabel('NMF component',fontsize=myfs)
+    plt.ylabel(Category_str,fontsize=myfs)
+    plt.yticks(np.arange(Ncats), CategoryType[0:20], rotation='horizontal',fontsize=myfs)
+    plt.xticks(np.arange(NMFCOMPS), (np.arange(NMFCOMPS)+1).astype(str), rotation='vertical',fontsize=myfs)
+    cbar = plt.colorbar(fraction=0.046, pad=0.04)
+    cbar.set_label(r'- $\log_{10} (p*$'+str(NMFCOMPS)+r'$*$'+str(Ncats)+r'$)$',fontsize=myfs)
+    cbar_ax = cbar.ax
+    cbar_ax.tick_params(labelsize=myfs)
+    for i in cbar_ax.get_yticklabels():
+        i.set_fontsize(myfs)
     if(save):
-        plt.savefig(today+Category_str+'MWplotNC'+str(NMFCOMPS)+filename_addon+'.pdf')
+        plt.savefig(filename_addon +  Category_str+'MWplot.pdf', bbox_inches='tight')
     else:
         plt.show()
 
@@ -90,26 +113,31 @@ def make_significance_plot_homogeneity(X, homogeneity, Category_str, my_rosetta,
     plt.clf()
     plt.figure(figsize=(len(CategoryType[0:20])*2,6))
     plt.imshow(CategoryChartMatrix.T, cmap=thecmap, vmin=-3, vmax=15)
-    #plt.xlabel('NMF component',fontsize=25)
+
     plt.xlabel(Category_str,fontsize=25)
     plt.xticks(np.arange(Ncats), CategoryType[0:20], rotation='vertical',fontsize=25)
-    #plt.xticks(np.arange(NMFCOMPS), np.arange(NMFCOMPS).astype(str), rotation='vertical',fontsize=25)
-    cbar = plt.colorbar()
+
+    cbar = plt.colorbar(fraction=0.046, pad=0.04, ticklabel_size=24)
     cbar.set_label(r'- $\log_{10} (p*$'+str(Ncats)+r'$)$',fontsize=25)
+    cbar_ax = cbar.ax
+    cbar_ax.tick_params(labelsize=35)
+    for i in cbar_ax.get_yticklabels():
+        i.set_fontsize(35)
+
     if(save):
-        plt.savefig(today+Category_str+'homogMWplotNC'+str(NMFCOMPS)+filename_addon+'.pdf')
+        plt.savefig(filename_addon +  Category_str+'MWhom_plot.pdf')
     else:
         plt.show()
 
     return (CategoryChartMatrix, CategoryType[0:20])
-#come back here
+
 
 def get_rosetta(MetaDataMat, names):
     import re
     DSnos = []
     DSnos_naked = []
     for name in names:
-        p = re.search('(_)(DS\w+)', name)
+        p = re.search('(.)(DS\w+)', name)
         DSnos.append(p.group(2))
         DSnos_naked.append(p.group(2)[2:])
     rosetta = []
